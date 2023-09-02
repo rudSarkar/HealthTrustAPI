4c62783f9b1173753800d88a61f14604ae8aabe7
Rudra Sarkar <rudrasarkar815@gmail.com>
2023-09-02 19:46:49 +0600

    fixed authorization issue appointment and removed default content

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -15,62 +15,6 @@ use Illuminate\Support\Facades\Validator;
 class PatientController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Patient $patient)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Patient $patient)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Patient $patient)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Patient $patient)
-    {
-        //
-    }
-
-    /**
      * Custom Login
      */
     public function login(Request $request)
@@ -147,6 +91,8 @@ class PatientController extends Controller
             $myInformation->dob = $request->dob;
             $myInformation->location_id = $request->location_id;
             $myInformation->user_id = auth()->user()->id;
+            $myInformation->phone_number = $request->phone_number;
+            $myInformation->blood_group = $request->blood_group;
             $myInformation->save();
 
             return response()->json(['message' => 'Information added successfully'], 200);
@@ -212,7 +158,7 @@ class PatientController extends Controller
         $doctor = DoctorAppointment::where('doctor_id', $request->doctor_id)->count();
 
         if($doctor == 10) {
-            return response()->json(['error' => 'can not take more appointment'], 200);
+            return response()->json(['error' => 'Can not take more appointment'], 200);
         }
 
         $doctor = User::with('doctor.location')
@@ -246,7 +192,7 @@ class PatientController extends Controller
     public function change_appointment_status_cancel(Request $request) {
         $appointmentId = $request->input('appointment_id');
 
-        $appointment = DoctorAppointment::find($appointmentId);
+        $appointment = DoctorAppointment::where('user_id', auth()->user()->id)->find($appointmentId);
 
         if(!$appointment) {
             return response()->json(['message' => 'Appointment not found'], 404);
